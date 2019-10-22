3dcc4a031bd71705585aae9e524930b4a3f41c41
LYROL LORENZO (GEDI-ISD-OOCLL/MNL) <LORENLY@OOCL.COM>
2019-10-22 18:41:09 +0800

    [Lyrol] Todo add features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
-import React from 'react';
+import React, {Component} from 'react'
+import uuid from 'uuid'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    content: '',
+    todos: [
+      
+    ]
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.content !== '') {
+      this.state.todos.push({
+        id: uuid.v4(),
+        content: this.state.content,
+        status: false
+      })
+    }
+    console.log(this.state.todos)
+  }
+
+  onChange = (e) => {
+    this.setState({content: e.target.value})
+    console.log(this.state.content)
+  }
+
+
+  render() {
+
+    const headerStyle = {
+      fontSize: '80px',
+    }
+
+    const wrapperStyle = {
+      backgroundColor: '#000',
+      color: 'white',
+      textAlign: 'center'
+    }
+
+    const inputStyle = {
+      width:'100%',
+      height: '50px',
+      fontSize: '30px',
+      textAlign: 'center'
+    }
+
+    const btnStyle = {
+      width:'100%',
+      height: '50px',
+    }
+
+    return (
+      <div style = {wrapperStyle}>
+        <h1 style = {headerStyle}>TODO TW</h1>
+        
+        <form onSubmit = {this.onSubmit}>
+          <input onChange = {this.onChange} style = {inputStyle} type = "text"></input>
+          <button style = {btnStyle} type = "submit">ADD TODO</button>
+        </form>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
